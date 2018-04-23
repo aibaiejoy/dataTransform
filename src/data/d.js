c3c1b034a49fcc1fa506ed11ce9457b67ad5a403
aibaiejoy <115176236@163.com>
2018-04-23 15:56:21 +0800

    复杂数据结构处理

--- src/data/d.js
+++ src/data/d.js
@@ -1,4 +1,12 @@
-import { transformData } from "../data";
+import React from "react";
+import { render } from "react-dom";
+import Hello from "./Hello";
+
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
 const data = {
   status: "SUCCESS",
   cause: null,
@@ -210,4 +218,46 @@ const data = {
   }
 };
 
-// console.log(transformData(data.data));
+const arr = ["1", "1", "1", "2", "2", "2", "3", "3", "3"];
+const aa = arr.reduce((newArr, item) => {
+  if (newArr.indexOf(item) === -1) {
+    newArr.push(item);
+  }
+  return newArr;
+}, []);
+console.log(aa);
+const object = data.data;
+
+const transformData = () => {
+  let newArr = [];
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      const a2 = object[key].map(item => {
+        item[key] = item.key_valuse;
+        delete item.key_valuse;
+        return item;
+      });
+      newArr = newArr.concat(a2);
+    }
+  }
+
+  const o4 = newArr.reduce((o3, item) => {
+    if (o3[item.f_type]) {
+      o3[item.f_type] = { ...o3[item.f_type], ...item };
+    } else {
+      o3[item.f_type] = item;
+    }
+    return o3;
+  }, {});
+  return Object.values(o4);
+};
+
+console.log(transformData(data.data));
+const App = () => (
+  <div style={styles}>
+    <Hello name="CodeSandbox" />
+    <h2>Start editing to see some magic happen {"\u2728"}</h2>
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
